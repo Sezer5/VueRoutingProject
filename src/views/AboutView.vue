f0bd4cdf4f338cbf1fab8b639950c1aa9d373748
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2025-10-31 16:36:44 +0300

    Yeni Route Eklenmesi

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About Page</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut placeat at porro consequatur omnis vel nesciunt quae quaerat. Numquam magnam, earum blanditiis quibusdam maxime aut magni corporis fuga saepe est!</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet provident veritatis hic, nobis accusantium a. Veritatis laborum, mollitia sapiente a quasi in sequi suscipit expedita ea! Optio fugiat corrupti neque.</p>
   </div>
 </template>
--- src/views/StudentsView.vue
+++ src/views/StudentsView.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="home">
-      <h2>Ana Sayfa</h2>
+      <h2>Öğrencilerim</h2>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non ut inventore reprehenderit, dolor sit ratione illo assumenda laboriosam quod, cum voluptates dolores blanditiis nisi molestias reiciendis in quaerat! Nam, dolores.</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus nam at ipsam neque iste dolor vero. Sed reiciendis exercitationem eos, deserunt vel nisi ipsa vero quos voluptatum itaque, maxime molestias.</p>
+      <div v-for="student in students" key="id">
+          {{student.name }}
+      </div>
   </div>
 </template>
 
@@ -10,9 +13,22 @@
 
 
 export default {
-  name: 'HomeView',
+  name: 'StudentsView',
   components: {
     
+  },
+  data(){
+    return{
+      students:[
+        {
+          name:'Sezer',
+          id:1
+        },{
+          name:'Seda',
+          id:2
+        }
+      ]
+    }
   }
 }
 </script>
